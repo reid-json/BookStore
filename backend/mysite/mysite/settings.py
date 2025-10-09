d71d3101e78f0cc03cb797fd8c08c6767419dafe
reid-json <jasonreidjr1@gmail.com>
2025-10-09 18:42:17 -0500

    scaffolding update

--- backend/mysite/mysite/settings.py
+++ backend/mysite/mysite/settings.py
@@ -37,6 +37,11 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'books',
+    'cart',
+    'orders',
+    'pages',
+    'search',
 ]
 
 MIDDLEWARE = [
