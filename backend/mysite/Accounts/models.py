d5dc5d0968234ee2634dee52326fd0e03ecca927
Lucas Bramer <lpb35330@ucmo.edu>
2025-10-20 10:11:46 -0500

    nothing

---
+++ backend/mysite/Accounts/models.py
@@ -0,0 +1,12 @@
+#imports the models function from django
+from django.db import models
+
+from mysite import Cart
+
+
+#creates a table named Accounts_Model with certain attributes (username, password, cart).
+#python version of sql kinda.
+class AccountsModel(models.Model):
+    username = models.CharField(max_length=100)
+    password = models.CharField(max_length=100)
+    cart = models.ForeignKey(Cart, on_delete=models.CASCADE)
--- backend/mysite/mysite/settings.py
+++ backend/mysite/mysite/settings.py
@@ -37,11 +37,12 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'books',
-    'cart',
-    'orders',
-    'pages',
-    'search',
+    'Books',
+    'Cart',
+    'mysite.Orders',
+    'mysite.Pages',
+    'mysite.Search',
+    'Accounts'
 ]
 
 MIDDLEWARE = [
