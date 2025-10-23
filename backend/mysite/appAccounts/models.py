bc25ab3085d82304f9dc1476724e760246ac9cf1
dwynn38 <dakotawynn99@gmail.com>
2025-10-22 19:04:40 -0500

    models stuff

--- backend/mysite/appAccounts/models.py
+++ backend/mysite/appAccounts/models.py
@@ -3,13 +3,7 @@ from django.db import models
 
 # Create your models here.
 
-class User(AbstractUser):
-    class Role(models.TextChoices):
-        USER = 'user'
-        ADMIN = 'admin'
-
-    role = models.CharField(max_length=10, choices=Role.choices, default=Role.USER)
-
-    @property
-    def is_admin(self) -> bool:
-        return self.role == self.Role.ADMIN or self.is_staff
+class Users(AbstractUser):
+    username = models.CharField(max_length=20)
+    email = models.CharField(max_length=25)
+    password = models.CharField(max_length=25)
