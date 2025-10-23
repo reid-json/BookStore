8353dbfdbc1a08f5f23afda51fdca2b14e2aaa51
reid-json <jasonreidjr1@gmail.com>
2025-10-22 21:17:39 -0500

    covers working

--- backend/mysite/appBooks/views.py
+++ backend/mysite/appBooks/views.py
@@ -1,10 +1,23 @@
-from rest_framework.views import APIView
 from rest_framework.response import Response
+from rest_framework.views import APIView
+
 from .models import BooksModel
 from .serializers import BooksModelSerializer
 
 class BooksListView(APIView):
-    def get(self, request):
+    @staticmethod
+    def get(request):
         books = BooksModel.objects.all()
         serializer = BooksModelSerializer(books, many=True)
-        return Response(serializer.data)
+        serialized_data = serializer.data
+
+        for i, book in enumerate(books):
+            if book.cover_image:
+
+                image_path = book.cover_image.decode('utf-8') if isinstance(book.cover_image, bytes) else str(book.cover_image)
+                image_url = request.build_absolute_uri(f'/media/{image_path}')
+                serialized_data[i]['cover_image'] = image_url
+            else:
+                serialized_data[i]['cover_image'] = None
+
+        return Response(serialized_data)
