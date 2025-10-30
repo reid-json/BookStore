96f846bda3ea65af75709baedd9f2dc5da5eb703
Lucas Bramer <lpb35330@ucmo.edu>
2025-10-29 19:16:28 -0500

    Figured out auto generated id problem
    The autogenerated id's were just a simple fix of defining an attribute as the primary key ourselves and I also solved the composite primary key issue

--- backend/mysite/appCart/models.py
+++ backend/mysite/appCart/models.py
@@ -5,5 +5,12 @@ from appBooks.models import BooksModel
 
 # Create your models here.
 class CartModel(models.Model):
-    username = models.ForeignKey(AccountModel, on_delete=models.CASCADE, primary_key=True)
+    username = models.ForeignKey(AccountModel, on_delete=models.CASCADE, related_name='cart_username')
+    email = models.ForeignKey(AccountModel, on_delete=models.CASCADE, related_name='cart_email')
     ispn = models.ForeignKey(BooksModel, on_delete=models.CASCADE)
+
+    class Meta:
+        constraints = [
+            models.UniqueConstraint(fields=['username', 'email', 'ispn'],
+                                    name='unique_cart')
+        ]
