775d92dea1f8675fd5378ff25a356e2cc5542270
Lucas Bramer <lpb35330@ucmo.edu>
2025-10-29 19:16:28 -0500

    Figured out auto generated id problem
    The autogenerated id's were just a simple fix of defining an attribute as the primary key ourselves and I also solved the composite primary key issue

--- backend/mysite/appAccounts/models.py
+++ backend/mysite/appAccounts/models.py
@@ -3,6 +3,12 @@ from django.db import models
 
 # Create your models here.
 class AccountModel(models.Model):
-    username = models.CharField(max_length=100, primary_key=True, serialize=False, unique=True, auto_created=True)
+    username = models.CharField(max_length=100)
     password = models.CharField(max_length=100)
-    email = models.EmailField(max_length=100, unique=True)
+    email = models.EmailField(max_length=100)
+
+    class Meta:
+        constraints = [
+            models.UniqueConstraint(fields=['username', 'email'],
+                                    name='unique_username_email')
+        ]
