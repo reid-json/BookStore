8a6874d1c6108f0478d87ba4a3c5f07e9c66be90
dwynn38 <dakotawynn99@gmail.com>
2025-11-05 21:45:57 -0600

    update

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -6,8 +6,8 @@ import Register from '@/views/Register.vue'
 
 const routes = [
     { path: '/', component: Home },
-    { path: '/Books', component: BooksPage },
-    { path: '/Login', component: Login },
+    { path: '/Books', name: 'Books',component: BooksPage },
+    { path: '/Login', name: 'Login',component: Login },
     { path: '/Register', name: 'Register', component: Register },
 ]
 
